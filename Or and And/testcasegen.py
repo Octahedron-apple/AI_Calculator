39df2ce57a187663a7067f622cde23ecd441a544
Octahedron-apple <bhavya1singh1@gmail.com>
2026-01-10 02:41:16 +0530

    Generate test cases for 'and' and 'or' operations

--- Or and And/testcasegen.py
+++ Or and And/testcasegen.py
@@ -1,1 +1,20 @@
-
+import random
+with open("test.txt","w") as f:
+    for i in range(40):
+        a=random.randint(0,1)
+        b=random.randint(0,1)
+        f.write(f"{a}and{b}={a and b}\n")
+    for i in range(40):
+        a=random.randint(0,1)
+        b=random.randint(0,1)
+        f.write(f"{a}or{b}={a or b}\n")
+    
+with open("dat.txt","w") as f:
+    for i in range(40):
+        a=random.randint(0,1)
+        b=random.randint(0,1)
+        f.write(f"{a}and{b}={a and b}\n")
+    for i in range(40):
+        a=random.randint(0,1)
+        b=random.randint(0,1)
+        f.write(f"{a}or{b}={a or b}\n")
