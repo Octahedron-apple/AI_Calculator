fbdd894fdadade2e3d35b7cbdd78fe8a05c9dff0
Octahedron-apple <bhavya1singh1@gmail.com>
2026-01-10 02:33:09 +0530

    Fix import statement for CountVectorizer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Tokenfrom sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 xt=[]
