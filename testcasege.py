71cd74f1e55f1c51ceba78fdcca8cb31e0453966
Octahedron-apple <bhavya1singh1@gmail.com>
2026-01-09 21:03:47 +0530

    Add random sum generation to testcasege.py
    Generate random sums and write to test.txt and dat.txt

--- testcasege.py
+++ testcasege.py
@@ -1,1 +1,11 @@
-
+import random
+with open("test.txt","w") as f:
+    for i in range(40):
+        for j in range(40):
+            if random.randint(1,2)==1:
+                f.write(f"{i}+{j}={i+j}\n")
+with open("dat.txt","w") as f:
+    for i in range(40):
+        for j in range(40):
+            if random.randint(1,2)==1:
+                f.write(f"{i}+{j}={i+j}\n")
