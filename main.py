a5e060ff72d1f39b97a568d8eb098027e8f6794c
Octahedron-apple <bhavya1singh1@gmail.com>
2026-01-09 21:12:33 +0530

    Implement data loading from files
    Added data loading functionality from dat.txt and test.txt.

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from sklearn.feature_extraction.text import CountVectorizer
+Tokenfrom sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
+xt=[]
+yt=[]
+xte=[]
+yte=[]
+d=0
+with open("dat.txt","r") as f:
+    a=f.readlines()
+for i in a:
+    xt.append(i.split("=")[0])
+    yt.append(i.split("=")[1])
+with open("test.txt","r") as f:
+    a=f.readlines()
+for i in a:
+    xte.append(i.split("=")[0])
+    yte.append(i.split("=")[1])
